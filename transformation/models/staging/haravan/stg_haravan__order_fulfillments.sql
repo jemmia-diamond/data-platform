952035d7916eb976f42c5d9652642cb38eed17ed
NhatNam1607 <nhatnam160701@gmail.com>
2026-05-05 18:01:25 +0700

    chore(dbt): update comments in haravan order fulfillments

--- transformation/models/staging/haravan/stg_haravan__order_fulfillments.sql
+++ transformation/models/staging/haravan/stg_haravan__order_fulfillments.sql
@@ -17,17 +17,11 @@ SELECT
     fulfillment->>'status' AS fulfillment_status,
     fulfillment->>'tracking_company' AS tracking_company,
     fulfillment->>'tracking_number' AS tracking_number,
-    
-    -- Ngày tháng quan trọng của vận đơn
     (fulfillment->>'created_at')::timestamp AS created_at,
     (fulfillment->>'ready_to_pick_date')::timestamp AS ready_to_pick_date,
     (fulfillment->>'delivered_date')::timestamp AS delivered_date,
-    
-    -- Trạng thái đơn vị vận chuyển
     fulfillment->>'carrier_status_name' AS carrier_status_name,
     fulfillment->>'carrier_cod_status_name' AS carrier_cod_status_name,
-    
-    -- Tiền thu hộ COD
     (fulfillment->>'cod_amount')::numeric AS cod_amount,
     (fulfillment->>'real_shipping_fee')::numeric AS real_shipping_fee
 
