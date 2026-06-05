210bf051c670d1f4b461ad89be1d6a3bd6da445f
NhatNam1607 <nhatnam160701@gmail.com>
2026-06-05 17:51:10 +0700

    fix(transformation): add missing adapter.commit() in view materialization macro

--- transformation/macros/materialization_view.sql
+++ transformation/macros/materialization_view.sql
@@ -10,9 +10,13 @@
 --   Full refresh: DROP CASCADE + CREATE  → handles column removals
 --
 {% materialization view, adapter='postgres' %}
-  {%- set target_relation = this.incorporate(type='view') -%}
   {%- set existing_relation = load_cached_relation(this) -%}
+  {%- set target_relation = this.incorporate(type='view') -%}
+  {% set grant_config = config.get('grants') %}
 
+  {{ run_hooks(pre_hooks, inside_transaction=False) }}
+
+  -- `BEGIN` happens here:
   {{ run_hooks(pre_hooks, inside_transaction=True) }}
 
   {%- if existing_relation is none -%}
@@ -41,9 +45,16 @@
     {%- endcall %}
   {%- endif -%}
 
-  {{ run_hooks(post_hooks, inside_transaction=True) }}
+  {% set should_revoke = should_revoke(existing_relation, full_refresh_mode=True) %}
+  {% do apply_grants(target_relation, grant_config, should_revoke=should_revoke) %}
 
   {% do persist_docs(target_relation, model) -%}
+
+  {{ run_hooks(post_hooks, inside_transaction=True) }}
+
+  {{ adapter.commit() }}
+
+  {{ run_hooks(post_hooks, inside_transaction=False) }}
 
   {{ return({'relations': [target_relation]}) }}
 {% endmaterialization %}
