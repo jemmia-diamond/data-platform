1abb510736c9bda7533c7bf35a1645dd6953f2d9
NhatNam1607 <nhatnam160701@gmail.com>
2026-06-09 11:11:42 +0700

    fix(transformation): use {% do %} instead of {{ }} in set_statement_timeout macro

--- transformation/macros/set_statement_timeout.sql
+++ transformation/macros/set_statement_timeout.sql
@@ -5,7 +5,7 @@
     SET statement_timeout = '{{ timeout_ms }}';
 {% endset %}
 
-{{ log("Setting statement_timeout to " ~ timeout_ms ~ "ms (" ~ (timeout_ms | int / 1000) ~ "s)", info=True) }}
-{{ run_query(sql) }}
+{% do log("Setting statement_timeout to " ~ timeout_ms ~ "ms (" ~ (timeout_ms | int / 1000) ~ "s)", info=True) %}
+{% do run_query(sql) %}
 
 {% endmacro %}
