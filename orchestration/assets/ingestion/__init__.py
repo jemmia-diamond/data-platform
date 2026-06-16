fe7303f84ba70b35b2467d20a6b72d09bbf08d00
NhatNam1607 <nhatnam160701@gmail.com>
2026-06-16 10:07:12 +0700

    feat(orchestration): add Google Sheets ingestion assets and daily schedules

--- orchestration/assets/ingestion/__init__.py
+++ orchestration/assets/ingestion/__init__.py
@@ -5,8 +5,15 @@ Ingestion layer Dagster asset wrappers.
 from .haravan import haravan_assets
 from .frappe import frappe_assets
 from .nocodb import nocodb_assets
+from .google_sheets import google_sheets_assets
 
-all_assets = [haravan_assets, frappe_assets, nocodb_assets]
+all_assets = [haravan_assets, frappe_assets, nocodb_assets, google_sheets_assets]
 
-__all__ = ["all_assets", "haravan_assets", "frappe_assets", "nocodb_assets"]
+__all__ = [
+    "all_assets",
+    "haravan_assets",
+    "frappe_assets",
+    "nocodb_assets",
+    "google_sheets_assets",
+]
 
