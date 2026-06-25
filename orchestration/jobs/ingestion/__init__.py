b2dc4a3b0b12ef968ea1b8cd808f217240d1e6c5
tintran270401 <tranbaotinpro@gmail.com>
2026-06-25 10:59:35 +0700

    feat(pancake): add Pancake job definitions and integrate into ingestion pipeline

--- orchestration/jobs/ingestion/__init__.py
+++ orchestration/jobs/ingestion/__init__.py
@@ -1,8 +1,9 @@
 from .frappe_erpnext import all_jobs as frappe_erpnext_jobs
 from .haravan import all_jobs as haravan_jobs
 from .nocodb import all_jobs as nocodb_jobs
+from .pancake import all_jobs as pancake_jobs
 
-all_jobs = haravan_jobs + frappe_erpnext_jobs + nocodb_jobs
+all_jobs = haravan_jobs + frappe_erpnext_jobs + nocodb_jobs + pancake_jobs
 
-__all__ = ["all_jobs", "frappe_erpnext_jobs", "haravan_jobs", "nocodb_jobs"]
+__all__ = ["all_jobs", "frappe_erpnext_jobs", "haravan_jobs", "nocodb_jobs", "pancake_jobs"]
 
---
+++ orchestration/jobs/ingestion/pancake.py
@@ -0,0 +1,7 @@
+from ...catalogs.ingestion.pancake import PANCAKE_EXECUTION_UNITS
+from ..common import build_jobs_by_name
+
+jobs_by_name = build_jobs_by_name(PANCAKE_EXECUTION_UNITS)
+all_jobs = tuple(jobs_by_name.values())
+
+__all__ = ["all_jobs", "jobs_by_name"]
