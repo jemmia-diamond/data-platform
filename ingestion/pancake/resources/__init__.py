a7767edd117cb83ecb77d8b87f970f13c2cdb5ee
tintran270401 <tranbaotinpro@gmail.com>
2026-07-07 15:40:28 +0700

    feat(pancake): add pages resource and integrate into ingestion pipeline

--- ingestion/pancake/resources/__init__.py
+++ ingestion/pancake/resources/__init__.py
@@ -1,4 +1,5 @@
 from .conversations import build_conversations
+from .pages import build_pages
 from .tables import TABLE_SPECS, build_table_resource
 
-__all__ = ["TABLE_SPECS", "build_conversations", "build_table_resource"]
+__all__ = ["TABLE_SPECS", "build_conversations", "build_pages", "build_table_resource"]
